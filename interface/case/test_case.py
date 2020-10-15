96db6b31a4e7e16a1f5be0e09efe1fecb5244b43
zhangxu0723 <zhangxu01@sinoiov.com>
2020-10-15 17:57:35 +0800

    1.修改请求的data格式

--- interface/case/test_case.py
+++ interface/case/test_case.py
@@ -30,3 +30,19 @@ class TestMethod(unittest.TestCase):
         res = self.run.run_main(url, method, data)
         logging.fatal(res)
         assert (res["errcode"] == 20009)
+
+    def test_03(self):
+        url = "https://test-ebs.utrailer.cn/api/web/order/list"
+        method = "post"
+        data = {"head": {"sequenceCode": "60472556-97f3-4229-51e1-6050535a07f3", "callType": "H5",
+                         "protocolVersion": "2.0", "appVersion": "2.0.0", "systemVersion": "11.2.2", "bizId": "7",
+                         "requestTime": 1602752911000, "mobileModel": "iPhone"},
+                "body": {"pageSize": 30, "pageNum": 1, "sortField": "", "sortOrder": "", "status": "ALL"},
+                "sign": "25991ba0237dc398cfa24e55201380ef"}
+        headers = {
+            "tokenid": "40e19653-ed79-4283-86d0-dda06654b3ac",
+            "content-type": "text/plain"
+        }
+        res = self.run.run_main(url, method, data, headers)
+        logging.fatal(res)
+        assert (res["head"]["errorMessage"] == "操作成功")
--- interface/utils/demo.py
+++ interface/utils/demo.py
@@ -1,18 +1,21 @@
+import json
+
 import requests
 
 
 class RunMain(object):
     @staticmethod
     def send_get(url, data, headers):
-        res = requests.get(url=url, data=data, headers=headers).json()
-        return res
+        res = requests.get(url=url, data=json.dumps(data), headers=headers)
+        return res.json()
 
     @staticmethod
     def send_post(url, data, headers):
-        res = requests.post(url=url, data=data, headers=headers).json()
-        return res
+        res = requests.post(url=url, data=json.dumps(data), headers=headers)
+        return res.json()
 
     def run_main(self, url, method, data=None, headers=None):
+        res = None
         if method.upper() == 'GET':
             res = self.send_get(url, data, headers)
         elif method.upper() == 'POST':
